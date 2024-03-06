b4ae3c834ed3bcd69b9e49eb4f9a239e675af0eb
jmpark24 <jmpark@gmail.com>
2024-03-06 15:27:24 +0900

    next.config.mjs에 nextConfig - images - domains 추가해서 cloudinary에서 이미지 불러와서 올려주기 성공

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      'res.cloudinary.com' // secure_url 에 들어오는 url 앞에 domain을 넣어주었다.
+    ]
+  }
+};
 
 export default nextConfig;
